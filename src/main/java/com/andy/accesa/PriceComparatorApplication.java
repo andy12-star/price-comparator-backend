efd00fadb5a682ae7e0ed7616eb2db570ea7d5d3
AndreeaIonita <ionitaandy@gmail.com>
2025-05-09 18:17:41 +0300

    added all the csv files to the project

--- src/main/java/com/andy/accesa/PriceComparatorApplication.java
+++ src/main/java/com/andy/accesa/PriceComparatorApplication.java
@@ -24,10 +24,10 @@ public class PriceComparatorApplication {
 	@Bean
 	public CommandLineRunner CsvLoader_test() {
 		return args -> {
-			Path path_products = Paths.get("src/main/resources/lidl_2025-05-08.csv");
+			Path path_products = Paths.get("src/main/resources/csv/products/lidl_2025-05-08.csv");
 			List<Product> products = CsvLoader.loadProducts(path_products, "lidl", LocalDate.of(2025, 5, 8));
 			products.forEach(System.out::println);
-			Path path_discounts = Paths.get("src/main/resources/lidl_discounts_2025-05-01.csv");
+			Path path_discounts = Paths.get("src/main/resources/csv/discounts/lidl_discounts_2025-05-01.csv");
 			List<Discount> discounts = CsvLoader.loadDiscounts(path_discounts, "lidl");
 			discounts.forEach(System.out::println);
 		};
